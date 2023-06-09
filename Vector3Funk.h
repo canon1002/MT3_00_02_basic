9892a1b52f2451a348770cd94fbe9fd3241f7aa6
canon1002 <k022g0038@g.neec.ac.jp>
2023-06-09 11:00:19 +0900

    MathToolクラス等を追加

--- Vector3Funk.h
+++ Vector3Funk.h
@@ -1,34 +1,32 @@
 #pragma once
 #include<Vector3.h>
+#include<cmath>
 #include<Novice.h>
+#include"MathTool.h"
 
 class Vector3Funk
 {
 public: //メンバ関数(メソッド)
 
-	static Vector3 Cross(const Vector3& v1, const Vector3& v2) {
-		
-		return Vector3{
-			(v1.y * v2.z) - (v1.z * v2.y),
-			(v1.z * v2.x) - (v1.x * v2.z),
-			(v1.x * v2.y) - (v1.y * v2.x),
-		};
 	
-	};
 
-	static float Dot(const Vector3& v1, const Vector3& v2) {
+	
 
-		return float{
-			v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
-		};
+	
 
-	};
-
-	static void VectorScreenPrintf(int posX, int posY, const Vector3& v1,const char* ravel) {
-
+	/// <summary>
+	/// 三次元行列の表示
+	/// </summary>
+	/// <param name="posX"></param>
+	/// <param name="posY"></param>
+	/// <param name="v1"></param>
+	/// <param name="ravel"></param>
+	static void VectorScreenPrintf(int posX, int posY, const Vector3& v1,const char* ravel) 
+	{
 		Novice::ScreenPrintf(posX, posY, "%s : %.2f,%.2f,%.2f,", ravel, v1.x, v1.y, v1.z);
-
 	}
+
+
 
 };
 
